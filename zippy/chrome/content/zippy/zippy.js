59874aac0846046a8616eee65a06f755c3dcbf7e
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2015-11-04 07:56:33 -0500

    cleaned up some code, added more sql

--- zippy/chrome/content/zippy/zippy.js
+++ zippy/chrome/content/zippy/zippy.js
@@ -8,7 +8,10 @@ Zotero.ZippyZotero = {
 		if (!this.DB.tableExists('links')) {
 			this.DB.query("CREATE TABLE links (id varchar(255), link varchar(255))");
 		}
-		
+
+		var links = this.DB.query("SELECT * FROM links;");
+		Zotero.debug(links);
+
 		// Register the callback in Zotero as an item observer
 		var notifierID = Zotero.Notifier.registerObserver(this.notifierCallback, ['item']);
 		
@@ -25,7 +28,7 @@ Zotero.ZippyZotero = {
 		var groupObjs = Zotero.Groups.getAll();
 		var groups = [];
 		
-		for (i = 0; i < groupObjs.length; i++) {
+		for (i = 0; i < groupObjs.length; i++) { 
 			groups.push(groupObjs[i].name);
 		}
 
@@ -52,12 +55,8 @@ Zotero.ZippyZotero = {
 		}
 	},
 
-	// For now: sync personal -> group, figure out the other way later
-	syncRecords: function(personalRecord, groupRecord) {
-		
-	},
-	
 	// This is literally the same function from Zotero's collectionTreeView.js
+	// TODO: Clean up, see if we need all ~150 lines of this
 	copyItem: function(item, targetLibraryID) {
 		// Check if there's already a copy of this item in the library
 		var linkedItem = item.getLinkedItem(targetLibraryID);
@@ -207,88 +206,35 @@ Zotero.ZippyZotero = {
 		return newID;
 	},
 
-	/*insertHello: function() {
-		var data = {
-			title: "Zotero",
-			company: "Center for History and New Media",
-			creators: [
-				['Dan', 'Stillman', 'programmer'],
-				['Simon', 'Kornblith', 'programmer']
-			],
-			version: '1.0.1',
-			company: 'Center for History and New Media',
-			place: 'Fairfax, VA',
-			url: 'http://www.zotero.org'
-		};
-		Zotero.Items.add('computerProgram', data); // returns a Zotero.Item instance
-	},*/
-	
-
-	//TODO: on event modification, check if it is linked, then propagate changes
+	// TODO: on event modification, check if it is linked, then propagate changes
 	// Callback implementing the notify() method to pass to the Notifier
 	notifierCallback: {
 		notify: function(event, type, ids, extraData) {
-			if (event == 'add' || event == 'modify' || event == 'delete'|| event == 'link') {
-				// Increment a counter every time an item is changed
-				Zotero.ZippyZotero.DB.query("UPDATE changes SET num = num + 1");
-				
-				if (event != 'delete') {
-					// Retrieve the added/modified items as Item objects
-					var items = Zotero.Items.get(ids);
-				}
-				else {
-					var items = extraData;
-				}
-				
-				// Loop through array of items and grab titles
-				var titles = [];
+			Zotero.debug("@@@ we've been notified of a" + event + " with type " + type + " with ids " 
+				+ ids + " with extra data " + extraData);
+			if (event == 'modify') {
+				Zotero.debug("@@@ we know something has been modified")
+						
+				// Retrieve the added/modified items as Item objects
+				var items = Zotero.Items.get(ids);
+								
 				for each(var item in items) {
-					// For deleted items, get title from passed data
-					if (event == 'delete') {
-						titles.push(item.old.title ? item.old.title : '[No title]');
-					}
-					else {
-						titles.push(item.getField('title'));
-					}
-				}
-				
-				if (!titles.length) {
-					return;
-				}
-				
-				// Get the localized string for the notification message and
-				// append the titles of the changed items
-				var stringName = 'notification.item' + (titles.length==1 ? '' : 's');
-				switch (event) {
-					case 'add':
-						stringName += "Added";
-						break;
-						
-					case 'modify':
-						stringName += "Modified";
-						break;
-						
-					case 'delete':
-						stringName += "Deleted";
-						break;
+					// check if item.id is in links table
+					/*var syncedItems = Zotero.ZippyZotero.DB.query("SELECT link FROM links WHERE id='" + item.id + "';");*/
+					var syncedItems = Zotero.ZippyZotero.DB.query("SELECT * FROM links;");
 
-					case 'linked':
-						stringName += "Linked";
-						break;
-				}
+					if (syncedItems.length) {
+						Zotero.debug("@@@" + syncedItems);
+
+						// Update
+					} else {
+						Zotero.debug("@@@ Didn't find anything");
+					}
+
+				}								
 				
-				var str = document.getElementById('zippy-zotero-strings').
-					getFormattedString(stringName, [titles.length]) + ":\n\n" +
-					titles.join("\n");
 			}
 			
-			/*var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-					.getService(Components.interfaces.nsIPromptService);		
-
-			var check = {value: true};                   // default the checkbox to true
-
-			var result = ps.confirmCheck(null, "Confirm sync with folder", 
-				"Do you want to sync %file% with %grouplibrary%?", null, check);*/
 		}
 	}
 };
