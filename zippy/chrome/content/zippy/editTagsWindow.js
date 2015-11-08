4d90ef7ae024132a71a65c5e2e671956c7b0f927
Yadi Xing <yadi.xing@mail.utoronto.ca>
2015-11-08 16:06:03 -0500

    refresh every time after delete

--- zippy/chrome/content/zippy/editTagsWindow.js
+++ zippy/chrome/content/zippy/editTagsWindow.js
@@ -2,19 +2,20 @@ Zotero.ZippyEditTagsWindow = {
 
 	EditTagsWin: function(tags) {
 		var tagIDs = [];
+		var checklist = document.getElementById('tagsList');
     	for (var key in tags) {
         var tagadd = tags[key]['_id'];
         var checked = document.getElementById(tagadd).checked;
         if (checked){
           tagIDs.push(tagadd);
+          var checkbox = document.getElementById(tagadd);
+          var garbage = checklist.removeChild(checkbox);
         }
     }
-    alert(tagIDs);
     Zotero.DB.beginTransaction();
     Zotero.Tags.erase(tagIDs);
 	Zotero.Tags.purge(tagIDs);
-	Zotero.DB.commitTransaction()
-	
+	Zotero.DB.commitTransaction();
 
 	}
 
