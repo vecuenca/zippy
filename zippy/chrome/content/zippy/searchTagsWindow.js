a8e7bb554e3354dd5541b60b1924d4b6fd144590
scp001 <yadi.xing1231@gmail.com>
2015-11-09 13:30:43 -0500

    refresh list when a search key word is entered

--- zippy/chrome/content/zippy/searchTagsWindow.js
+++ zippy/chrome/content/zippy/searchTagsWindow.js
@@ -2,7 +2,20 @@ Zotero.ZippySearchTagsWindow = {
 
 	SearchTagsWin: function() {
 
-    alert("you searched for tags");
+    var tagvalue = document.getElementById('find-tag').value;
+
+    alert(tagvalue);
+
+    var tags = Zotero.Tags.getAll();
+
+    for (var tag in tags) {
+        var tagadd = tags[tag]['_id'];
+        
+        if (tagvalue != tags[tag]['_name']){
+          var checkbox = document.getElementById(tagadd);
+          var garbage = checklist.removeChild(checkbox);
+        }
+    }
 
 	}
 
