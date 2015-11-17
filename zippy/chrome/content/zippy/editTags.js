2a30cc7876aa528304d10e28f57334c3b3b5b267
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2015-11-16 23:33:10 -0500

    initial commit for item link dialog

--- zippy/chrome/content/zippy/editTags.js
+++ zippy/chrome/content/zippy/editTags.js
@@ -1,9 +1,27 @@
 Zotero.ZippyOpenDialogs = {
 
 	EditMultipleTags: function() {
-		var somefile=document.getElementById('zippy-zotero-strings').value;
+		var somefile = document.getElementById('zippy-zotero-strings').value;
 		var all_tags = Zotero.Tags.getAll();
 		window.openDialog("chrome://zippy/content/editTags.xul","editAllTags", "chrome",somefile, all_tags);
+	},
+
+	OpenItemLinksDiag: function() {
+		var somefile = document.getElementById('zippy-zotero-strings').value;
+		var linksTable = Zotero.ZippyZotero.DB.query("SELECT * from links;");
+
+		var linkedItems = [];
+	    for (var i = 0; i < linksTable.length; i++) {
+	        var srcItem = Zotero.Items.get(linksTable[i].id);
+	        var linkItem = Zotero.Items.get(linksTable[i].link);
+	        linkedItems.push({srcTitle: srcItem.getField("title"),
+	        	linkTitle: Zotero.Groups.getByLibraryID(linkItem.libraryID).name,
+	        	srcId: srcItem.id,
+	        	linkId: linkItem.id});
+	    }
+
+		window.openDialog("chrome://zippy/content/editLinks.xul", "editItemLinks",
+			"chrome", somefile, linkedItems);
 	}
 
 }
--- zippy/chrome/content/zippy/editTagsWindow.js
+++ zippy/chrome/content/zippy/editTagsWindow.js
@@ -2,22 +2,22 @@ Zotero.ZippyEditTagsWindow = {
 
 	EditTagsWin: function() {
     var tags = Zotero.Tags.getAll();
-		var tagIDs = [];
-		var checklist = document.getElementById('tagsList');
-    	for (var key in tags) {
-        var tagadd = tags[key]['_id'];
-        var checked = document.getElementById(tagadd).checked;
-        if (checked){
-          tagIDs.push(tagadd);
-          var checkbox = document.getElementById(tagadd);
-          var garbage = checklist.removeChild(checkbox);
-        }
+    var tagIDs = [];
+    var checklist = document.getElementById('tagsList');
+    for (var key in tags) {
+      var tagadd = tags[key]['_id'];
+      var checked = document.getElementById(tagadd).checked;
+      if (checked){
+        tagIDs.push(tagadd);
+        var checkbox = document.getElementById(tagadd);
+        var garbage = checklist.removeChild(checkbox);
+      }
     }
     Zotero.DB.beginTransaction();
     Zotero.Tags.erase(tagIDs);
-	Zotero.Tags.purge(tagIDs);
-	Zotero.DB.commitTransaction();
+    Zotero.Tags.purge(tagIDs);
+    Zotero.DB.commitTransaction();
 
-	}
+  }
 
 }
