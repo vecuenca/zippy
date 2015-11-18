c5a962993efe7048f8d11e3c24bddd4256c3104d
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2015-11-17 20:03:32 -0500

    implemeted link deletion feature

--- zippy/chrome/content/zippy/editLinksWindow.js
+++ zippy/chrome/content/zippy/editLinksWindow.js
@@ -1,8 +1,22 @@
 Zotero.ZippyEditLinksWindow = {
 
 	deleteLink: function() {
-		var checkList = document.getElementById("itemList");
+		var itemList = document.getElementById("linkList");
+		var items = document.getElementsByClassName("listItem");
 
-		/*var checkboxes = document.getElementsB*/
+		for (var i=0; i<items.length; i++) {
+			if (items[i].selected) {
+				var result = window.confirm("Are you sure you want to delete this link?");
+				if (result) {
+					//run the query to delete the link
+					Zotero.ZippyZotero.DB.query("DELETE FROM links WHERE id='"
+						+ items[i].firstChild.getAttribute("srcId") + "' AND link='"
+						+ items[i].lastChild.getAttribute("linkId") + "';");
+
+					//remove row from the DOM
+					itemList.removeChild(items[i]);
+				}
+			}
+		}
 	}
 }
