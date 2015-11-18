1b5af5ea91b91ad9bf6c30c3b43407ece140be21
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2015-11-18 09:49:54 -0500

    trying out trees instead of richlistbox

--- zippy/chrome/content/zippy/editLinksWindow.js
+++ zippy/chrome/content/zippy/editLinksWindow.js
@@ -5,16 +5,15 @@ Zotero.ZippyEditLinksWindow = {
 		var items = document.getElementsByClassName("listItem");
 
 		for (var i=0; i<items.length; i++) {
-			if (items[i].selected) {
+			var tableRow = items[i];
+			if (tableRow.selected) {
 				var result = window.confirm("Are you sure you want to delete this link?");
 				if (result) {
-					//run the query to delete the link
 					Zotero.ZippyZotero.DB.query("DELETE FROM links WHERE id='"
-						+ items[i].firstChild.getAttribute("srcId") + "' AND link='"
-						+ items[i].lastChild.getAttribute("linkId") + "';");
+						+ tableRow.firstChild.getAttribute("srcId") + "' AND link='"
+						+ tableRow.lastChild.getAttribute("linkId") + "';");
 
-					//remove row from the DOM
-					itemList.removeChild(items[i]);
+					itemList.removeChild(tableRow);
 				}
 			}
 		}
