90558db31c7c9175a4410d62ecac2469b3aef7c0
Yadi Xing <yadi.xing@mail.utoronto.ca>
2015-11-17 22:48:38 -0500

    add database query for adding new field

--- zippy/chrome/content/zippy/addField.js
+++ zippy/chrome/content/zippy/addField.js
@@ -1,12 +1,31 @@
 Zotero.ZippyAddField = {
 
+	DB: null,
+
+	init: function() {
+		// Connect to (and create, if necessary) zippy.sqlite in the Zotero directory
+		this.DB = new Zotero.DBConnection("zippy");
+
+		if (!this.DB.tableExists("fields")) {
+			this.DB.query("CREATE TABLE fields (id varchar(255), field varchar(255), content varchar(255))");
+		}
+	},
+
+
 	AddField: function(items) {
-		alert(items.toSource()); 
+		this.DB = new Zotero.DBConnection("zippy");
 		for (i = 0; i < items.length; i++) {
 				var item = items[i];
 				alert(item.id);
-				// use item.id to do the query
+				var fieldname = document.getElementById("enter-name");
+				var content = document.getElementById("enter-content");
+				this.DB.query("INSERT INTO fields (id, field, content) VALUES (" + item.id + "," + fieldname +  "," + content + ")";
+
 			}
 	}
 
 }
+
+window.addEventListener("load", function(e) {
+	Zotero.ZippyAddField.init();
+}, false);
