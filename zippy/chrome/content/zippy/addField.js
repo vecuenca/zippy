668aa6b69835b573f39f1e3d663fabc278643ab6
zhoubecky <xiaoh.zhou@mail.utoronto.ca>
2015-11-21 22:06:54 -0500

    create fields in db

--- zippy/chrome/content/zippy/addField.js
+++ zippy/chrome/content/zippy/addField.js
@@ -1,31 +1,30 @@
+/* jshint shadow:true */
+"use strict";
 Zotero.ZippyAddField = {
 
 	DB: null,
 
+
 	init: function() {
-		// Connect to (and create, if necessary) zippy.sqlite in the Zotero directory
 		this.DB = new Zotero.DBConnection("zippy");
 
 		if (!this.DB.tableExists("fields")) {
-			this.DB.query("CREATE TABLE fields (id varchar(255), field varchar(255), content varchar(255))");
+			this.DB.query("CREATE TABLE fields (id varchar(255), field varchar(255),content varchar(255))");
 		}
 	},
 
 
 	AddField: function(items) {
-		this.DB = new Zotero.DBConnection("zippy");
-		for (i = 0; i < items.length; i++) {
+		for (var i = 0; i < items.length; i++) {
 				var item = items[i];
-				alert(item.id);
-				var fieldname = document.getElementById("enter-name");
-				var content = document.getElementById("enter-content");
-				this.DB.query("INSERT INTO fields (id, field, content) VALUES (" + item.id + "," + fieldname +  "," + content + ")";
-
+				var fieldname = document.getElementById('enter-name').value;
+				var content = document.getElementById('enter-content').value;
+				this.DB.query("INSERT INTO fields (id, field, content) VALUES (" + item.id + "," + fieldname +  "," + content + ")");
 			}
 	}
-
 }
 
+// Initialize the utility
 window.addEventListener("load", function(e) {
 	Zotero.ZippyAddField.init();
 }, false);
